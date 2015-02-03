0770c5274e46a1f25b6ca0fbd235ffeceea7d43f
Kyle Squizzato <ksquizz@gmail.com>
2015-02-03 09:57:56 -0500

    We now manipulate text by adding a space in front of both lines

--- grabh1.py
+++ grabh1.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 from bs4 import BeautifulSoup
+from time import sleep
 import urllib2
 
 text = clipboard.get_selection()
 html = urllib2.urlopen(text).read()
 soup = BeautifulSoup(html)
 soup.prettify()
-keyboard.send_key("<up>")
+keyboard.send_key('<up>')
 keyboard.send_keys(soup.h1.string)
+# We need to wait a moment for the string to get pulled from url before manipulating text
+sleep(1)
+keyboard.send_keys('<home> <down><home> ')
